deac85858a7c10ef92cec93e9951fcf3e9370e0d
Zoe Zhang <yunqiz@berkeley.edu>
2019-04-23 01:15:43 -0700

    proj3

--- byow/Core/Room.java
+++ byow/Core/Room.java
@@ -2,12 +2,16 @@ package byow.Core;
 
 import byow.TileEngine.TERenderer;
 import byow.TileEngine.TETile;
+import byow.TileEngine.Tileset;
+
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Room {
-    private Position p; //upper left corner of the room
-    private int width;
-    private int height;
+    private Position p; //bottom left corner of the room, wall
+    private int width;  //including the wall
+    private int height; //including the wall
+    private int maxSize; // height/width = 6
 
     public Room(Position p, int w, int h) {
         this.p = p;
@@ -15,23 +19,48 @@ public class Room {
         this.height = h;
     }
 
-    public Position p() {
-        return this.p;
-    }
-
-    public int h() {
-        return this.height;
-    }
-
-    public int w() {
-        return this.width;
-    }
 
     public void addRoom(TETile[][] world, Position p, int w, int h) {
+        for (int i = 0; i < w; i++ ) {
+            world[p.x() + i][p.y()] = Tileset.WALL;
+            world[p.x() + i][p.y() + h - 1] = Tileset.WALL;
+        }
 
+        for (int j = 0; j < h; j++) {
+            world[p.x()][p.y() + j] = Tileset.WALL;
+            world[p.x() + w - 1][p.y() + j] = Tileset.WALL;
+        }
     }
 
-    public void roomFloor(TETile[][] world, Room r) {
-
+    private ArrayList<Position> roomSpace(Room r) {
+        ArrayList<Position> space = new ArrayList<>();
+        for (int i = 1; i < r.width - 1; i++) {
+            for (int j = 1; j < r.height - 1; j++) {
+                space.add(new Position(r.p.x() + i, r.p.y() + j));
+            }
+        }
+        return space;
     }
+
+    public Position ranPosInRoom(Room r) {
+        Random random = new Random();
+        int ranX = random.nextInt(r.width - 2) + r.p.x() + 1;
+        int ranY = random.nextInt(r.height - 2) + r.p.y() + 1;
+        return new Position(ranX, ranY);
+    }
+
+    public ArrayList<Room> sortedList(ArrayList<Room> list) {
+        ArrayList<Room> newList = new ArrayList<>();
+
+        return newList;
+    }
+
+    public void fillAll(TETile[][] world, ArrayList<Room> roomList) {
+        for (Room r : roomList) {
+            for (Position p : roomSpace(r)) {
+                world[p.x()][p.y()] = Tileset.FLOOR;
+            }
+        }
+    }
+
 }
